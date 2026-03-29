cbf664181bb73eedd32876026a830031b7179e16
iDavexxd <davidmagno2007@gmail.com>
2026-03-28 21:13:33 -0600

    kanye saco bully les gou

--- src/main/java/app/simplereader/SimpleReader.java
+++ src/main/java/app/simplereader/SimpleReader.java
@@ -4,6 +4,9 @@ import javafx.scene.control.Label;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,18 +18,33 @@ public class SimpleReader extends Application{
     
     @Override
     public void start(Stage stage) {
-        Button boton = new Button();
-        Label lb = new Label("XD");
-        boton.setText("Sexoo");
-        StackPane panel = new StackPane(boton,lb);
-        Scene escena = new Scene(panel,1280,720);
+        
+        Image image = new Image("file:/home/david/Imágenes/16b345f8-09e0-4160-ad0b-7a5b511e5501.jpeg");
+        ImageView visor = new ImageView(image);
+        
+        Button btnNext = new Button("->");
+        Button btnBack = new Button("<-");
+        btnNext.setOnAction(e -> {
+            Logger.Log("Cliqueaste siguiente!");
+        });
+        btnBack.setOnAction(e -> Logger.Log("Cliqueaste volver!"));
+        
+        visor.setFitHeight(400);
+        visor.setFitWidth(600);
+        visor.setPreserveRatio(true);
+        
+        BorderPane layout = new BorderPane();
+        layout.setCenter(visor);
+        layout.setLeft(btnBack);
+        layout.setRight(btnNext);
         
         
+        Scene escena = new Scene(layout,1280,720);
+               
         stage.setTitle("Simple Reader");
         stage.setScene(escena);
-        stage.show();
         stage.setResizable(false);
-        
+        stage.show();
     }
 
     public static void main(String[] args) {
