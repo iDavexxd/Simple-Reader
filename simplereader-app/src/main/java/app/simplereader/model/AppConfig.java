11948cff6bf5de272b5e947edd7227a0ce4537c5
iDavexxd <davidmagno2007@gmail.com>
2026-06-27 23:34:26 -0600

    fixito 2

--- simplereader-app/src/main/java/app/simplereader/model/AppConfig.java
+++ simplereader-app/src/main/java/app/simplereader/model/AppConfig.java
@@ -19,7 +19,7 @@ public class AppConfig {
     public int HEIGHT = 720;
     public static String APP_TITLE = "Simple Reader";
     
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "v1.1.0";
 
     
     public String READING_DIR = "LTR";
